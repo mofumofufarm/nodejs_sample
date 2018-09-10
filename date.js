277b876a8a31babff9adcab8bdde38ddcb5e2cbb
モフモフ <42226397+mofumofufarm@users.noreply.github.com>
2018-09-10 18:25:14 +0900

    Update date.js
    ファイル書き込みについて保存

--- date.js
+++ date.js
@@ -6,3 +6,8 @@ written by moufumofufarm
 var dt = new Date();
 dt.setTime(dt.getTime() + 1000*60*60*9);
 console.log(dt);
+
+//ファイルをasync保存
+var dt = new Date();
+var jsonfilename = `./board/${dt.getTime()}.json`
+fs.writeFileSync(jsonfilename,JSON.stringify(board));
